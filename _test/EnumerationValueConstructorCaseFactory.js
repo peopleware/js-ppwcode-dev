54a34344258e72360cb58d9d0cf8932a6221d335
Jan Dockx <Jan_Dockx@peopleware.be>
2014-09-11 14:53:41 +0200

    Squashed 'src/lib/ppwcode/vernacular/semantics-B/' content from commit e98bb1e
    git-subtree-dir: src/lib/ppwcode/vernacular/semantics-B
    git-subtree-split: e98bb1efdacb4800d906f5b35b629beb12c35a62

---
+++ _test/EnumerationValueConstructorCaseFactory.js
@@ -0,0 +1,99 @@
+/*
+ Copyright 2014 - $Date $ by PeopleWare n.v.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+define(["dojo/_base/declare", "../EnumerationValue",
+        "../_util/contracts/CaseFactory", "../_contract/EnumerationValueConstructor",
+        "module"],
+  function(declare, EnumerationValue,
+           CaseFactory, Contract,
+           module) {
+
+    var EnumerationValueStub1 = EnumerationValue.declare(
+      {},
+      ["first", "second", "third"],
+      module.id + "_Stub1"
+      // bundle is standard
+    );
+
+    return declare([CaseFactory], {
+
+      contract: new Contract(),
+
+      subjectFactories: function() {
+        return [function() {return EnumerationValue;}];
+      },
+
+      $declare: function() {
+        return [
+          this.subjectFactories(),
+          {
+            name: "SuperType",
+            canBeSkipped: true,
+            factories: [
+              function() {return EnumerationValueStub1;}
+            ]
+          },
+          {
+            name: "prototypeDef",
+            factories: [
+              undefined,
+              null,
+              {},
+              {someProperty: "SOME PROPERTY VALUE"}
+            ]
+          },
+          {
+            name: "valueDefinitions",
+            factories: [
+              undefined,
+              null,
+              [],
+              {},
+              ["alpha", "beta", "gamma"],
+              [
+                {representation: "1", someProp: 5},
+                {representation: "2", someProp: 5},
+                {representation: "3", someProp: 7, someOtherProp: new Date()}
+              ],
+              {
+                one: {representation: "1", someProp: 5},
+                two: {representation: "2", someProp: 5},
+                three: {representation: "3", someProp: 7, someOtherProp: new Date()}
+              }
+              // precondition violation ["aleph", "aleph"],
+              /* precondition violation
+              {
+                blue: {representation: "0000FF"},
+                two: {representation: "0000FF"}
+              }
+              */
+            ]
+          },
+          {
+            name: "mod",
+            factories: [undefined, null, "", {id: "someMid"}, "aMid"]
+          },
+          {
+            name: "bundleName",
+            factories: [undefined, null, "", "ABundleName"]
+          }
+        ];
+      }
+
+    });
+
+  }
+);
---
+++ _test/EnumerationValueTypeCaseFactory.js
@@ -0,0 +1,81 @@
+/*
+ Copyright 2012 - $Date $ by PeopleWare n.v.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+define(["dojo/_base/declare", "./TransformerCaseFactory", "../_contract/EnumerationValueType"],
+ function(declare, TransformerCaseFactory, Contract) {
+
+   return declare([TransformerCaseFactory], {
+
+     constructor: function(kwargs) {
+       this.contract = new Contract({SubjectType: kwargs.SubjectType});
+     },
+
+     jsonFactories: function() {
+       return {
+         name: "json",
+         factories: [
+           null,
+           undefined,
+           "",
+           {
+             name: "2 quotes",
+             factory: "\"\""
+           },
+           {
+             name: "valid but quoted",
+             factory: "\"first\""
+           },
+           {
+             name: "not a value",
+             factory: "XX not a JSON value XX"
+           },
+           {
+             name: "not a value, quoted",
+             factory: "\"XX not a JSON value XX\""
+           }
+         ].concat(this.valueFactories().map(function(factoryOrConstant) {
+           var value = typeof factoryOrConstant === "function" ? factoryOrConstant() : factoryOrConstant;
+           return function() {return value.toJSON();};
+         }))
+       };
+     },
+
+     $values: function() {
+       return [this.subjectFactories()];
+     },
+
+     $isJson: function() {
+       return [this.subjectFactories(), this.jsonFactories()];
+     },
+
+     $revive: function() {
+       return [this.subjectFactories(), this.jsonFactories()];
+     },
+
+     $getBundle: function() {
+       return [
+         this.subjectFactories(),
+         {
+           name: "lang",
+           factories: [null, undefined, "", "nl", "ru"]
+         }
+       ];
+     }
+
+   });
+
+ }
+);
